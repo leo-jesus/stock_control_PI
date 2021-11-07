87d318c34290612ac651231c4e47d1e17f468754
leo-jesus <llxdgb1@gmail.com>
2021-11-07 10:12:06 -0300

    update balance of each product

--- stock_control/produto/urls.py
+++ stock_control/produto/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.produto_list, name='produto_list'),
     path('<int:pk>/', views.produto_detail, name='produto_detail'),
     path('add/', views.ProdutoCreate.as_view(), name='produto_add'),
-    path('<int:pk>', views.ProdutoUpdate.as_view(), name='produto_edit'),
+    path('<int:pk>/edit/', views.ProdutoUpdate.as_view(), name='produto_edit'),
+    path('<int:pk>/json/', views.produto_json, name='produto_json'),
 ]
