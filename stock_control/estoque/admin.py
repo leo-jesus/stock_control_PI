fc050b8c82385aee2ca5bb0ab64a10c7db29a5cc
leo-jesus <llxdgb1@gmail.com>
2021-11-07 10:49:28 -0300

    created proxy model

--- stock_control/estoque/admin.py
+++ stock_control/estoque/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Estoque, EstoqueItens
+from .models import EstoqueEntrada, EstoqueSaida, EstoqueItens
 
 
 class EstoqueItensInLine(admin.TabularInline):
@@ -7,8 +7,17 @@ class EstoqueItensInLine(admin.TabularInline):
     extra = 0
 
 
-@admin.register(Estoque)
-class EstoqueAdmin(admin.ModelAdmin):
+@admin.register(EstoqueEntrada)
+class EstoqueEntradaAdmin(admin.ModelAdmin):
+    inlines = (EstoqueItensInLine, )
+    list_display = ('__str__', 'nf', 'funcionario')
+    search_fieds = ('nf',)
+    list_filter = ('funcionario',)
+    date_hierarchy = 'created'
+
+
+@admin.register(EstoqueSaida)
+class EstoqueSaidaAdmin(admin.ModelAdmin):
     inlines = (EstoqueItensInLine, )
     list_display = ('__str__', 'nf', 'funcionario')
     search_fieds = ('nf',)
