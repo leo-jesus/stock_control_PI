d30173938c26aa344e199d34b99c78718974f31d
leo-jesus <llxdgb1@gmail.com>
2021-11-08 13:14:25 -0300

    changes in list stock

--- stock_control/estoque/urls.py
+++ stock_control/estoque/urls.py
@@ -6,11 +6,11 @@ app_name = 'estoque'
 
 
 urlpatterns = [
-    path('', views.estoque_entrada_list, name='estoque_entrada_list'),
+    path('', views.EstoqueEntradaList.as_view(), name='estoque_entrada_list'),
     path('<int:pk>/', views.estoque_entrada_detail,
          name='estoque_entrada_detail'),
     path('add/', views.estoque_entrada_add, name='estoque_entrada_add'),
-    path('saida/', views.estoque_saida_list, name='estoque_saida_list'),
+    path('saida/', views.EstoqueSaidaList.as_view(), name='estoque_saida_list'),
     path('saida/<int:pk>/', views.estoque_saida_detail,
          name='estoque_saida_detail'),
     path('saida/add/', views.estoque_saida_add, name='estoque_saida_add'),
--- stock_control/estoque/views.py
+++ stock_control/estoque/views.py
@@ -2,21 +2,21 @@ from django.forms import inlineformset_factory
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, resolve_url
 from .models import Estoque, Produto
+from django.views.generic import ListView
 
 from stock_control.estoque.forms import EstoqueForm, EstoqueItensForm
 from .models import EstoqueEntrada, EstoqueSaida, EstoqueItens
 
 
-def estoque_entrada_list(request):
+class EstoqueEntradaList(ListView):
+    model = EstoqueEntrada
     template_name = 'estoque_list.html'
-    objects = EstoqueEntrada.objects.all()
-    context = {
-        'object_list': objects,
-        'titulo': 'Entrada',
-        'url_add': 'estoque:estoque_entrada_add',
 
-    }
-    return render(request, template_name, context)
+    def get_context_data(self, **kwargs):
+        context = super(EstoqueEntradaList, self).get_context_data(**kwargs)
+        context["titulo"] = 'Entrada'
+        context["url_add"] = 'estoque:estoque_entrada_add'
+        return context
 
 
 def estoque_entrada_detail(request, pk):
@@ -76,15 +76,26 @@ def estoque_entrada_add(request):
     return render(request, template_name, context)
 
 
-def estoque_saida_list(request):
+# def estoque_saida_list(request):
+#     template_name = 'estoque_list.html'
+#     objects = EstoqueSaida.objects.all()
+#     context = {
+#         'object_list': objects,
+#         'titulo': 'Saída',
+#         'url_add': 'estoque:estoque_saida_add',
+#     }
+#     return render(request, template_name, context)
+
+
+class EstoqueSaidaList(ListView):
+    model = EstoqueSaida
     template_name = 'estoque_list.html'
-    objects = EstoqueSaida.objects.all()
-    context = {
-        'object_list': objects,
-        'titulo': 'Saída',
-        'url_add': 'estoque:estoque_saida_add',
-    }
-    return render(request, template_name, context)
+
+    def get_context_data(self, **kwargs):
+        context = super(EstoqueSaidaList, self).get_context_data(**kwargs)
+        context["titulo"] = 'Saída'
+        context["url_add"] = 'estoque:estoque_saida_add'
+        return context
 
 
 def estoque_saida_detail(request, pk):
