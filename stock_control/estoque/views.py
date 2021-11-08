823ea87d86ca5d2ed4a3f44a85017862aec219d9
leo-jesus <llxdgb1@gmail.com>
2021-11-08 12:40:35 -0300

    stock enter and outs unificared to one archive

--- stock_control/estoque/views.py
+++ stock_control/estoque/views.py
@@ -8,9 +8,14 @@ from .models import EstoqueEntrada, EstoqueSaida, EstoqueItens
 
 
 def estoque_entrada_list(request):
-    template_name = 'estoque_entrada_list.html'
+    template_name = 'estoque_list.html'
     objects = EstoqueEntrada.objects.all()
-    context = {'object_list': objects}
+    context = {
+        'object_list': objects,
+        'titulo': 'Entrada',
+        'url_add': 'estoque:estoque_entrada_add',
+
+    }
     return render(request, template_name, context)
 
 
@@ -69,9 +74,13 @@ def estoque_entrada_add(request):
 
 
 def estoque_saida_list(request):
-    template_name = 'estoque_saida_list.html'
+    template_name = 'estoque_list.html'
     objects = EstoqueSaida.objects.all()
-    context = {'object_list': objects}
+    context = {
+        'object_list': objects,
+        'titulo': 'Saída',
+        'url_add': 'estoque:estoque_saida_add',
+    }
     return render(request, template_name, context)
 
 
