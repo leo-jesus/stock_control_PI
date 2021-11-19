8fbed465fc19bc181275ef59028368a740b85fac
leo-jesus <llxdgb1@gmail.com>
2021-11-19 20:36:30 -0300

    set deploy

--- stock_control/settings.py
+++ stock_control/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 from decouple import config, Csv
 
@@ -135,3 +136,5 @@ LOGOUT_REDIRECT_URL = 'core:index'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
